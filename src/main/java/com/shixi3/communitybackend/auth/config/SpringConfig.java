23e013340d44bf61e8c1371a3bbfe776f7599798
lsh <3040114965@qq.com>
2024-07-02 11:09:23 +0800

    完善登录

--- src/main/java/com/shixi3/communitybackend/auth/config/SpringConfig.java
+++ src/main/java/com/shixi3/communitybackend/auth/config/SpringConfig.java
@@ -4,12 +4,13 @@ import com.shixi3.communitybackend.common.handler.SpringMVCExceptionHandler;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.web.servlet.DispatcherServlet;
 
-@AutoConfiguration
+@Configuration
 @ConditionalOnClass(DispatcherServlet.class)
-public class SpringMVCConfigure {
+public class SpringConfig {
     @Bean
     @Order(0)
     public SpringMVCExceptionHandler springMVCExceptionHandler() {
