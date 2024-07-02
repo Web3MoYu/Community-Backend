1b5a3ad3c9f4a29efbae075a5fa7ef9d5305aee7
徐世豪 <1304928447@qq.com>
2024-07-02 16:35:36 +0800

    获取个人车辆信息

---
+++ src/main/java/com/shixi3/communitybackend/car/mapper/CarMapper.java
@@ -0,0 +1,12 @@
+package com.shixi3.communitybackend.car.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.shixi3.communitybackend.car.entity.Car;
+import org.apache.ibatis.annotations.Mapper;
+
+
+@Mapper
+public interface CarMapper extends BaseMapper<Car> {
+
+    Car getCarByOwner(Long owner);
+}
---
+++ src/main/java/com/shixi3/communitybackend/car/service/CarService.java
@@ -0,0 +1,8 @@
+package com.shixi3.communitybackend.car.service;
+
+import com.shixi3.communitybackend.car.entity.Car;
+
+public interface CarService {
+
+    Car getCarByOwner(Long owner);
+}
