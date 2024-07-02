f4ec40e94b07e498af58151579fd2b7b71246dc3
lsh <3040114965@qq.com>
2024-07-02 09:40:21 +0800

    init

---
+++ src/test/java/com/shixi3/communitybackend/CommunityBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shixi3.communitybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
