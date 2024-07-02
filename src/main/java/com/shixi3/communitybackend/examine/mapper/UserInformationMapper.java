6461c01e2ccbf560b983849de60774bda7d91e64
Cx330I <2369562758@qq.com>
2024-07-02 17:25:38 +0800

    审核模块搭建

---
+++ src/main/java/com/shixi3/communitybackend/examine/mapper/UserInformationMapper.java
@@ -0,0 +1,13 @@
+package com.shixi3.communitybackend.examine.mapper;
+
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.shixi3.communitybackend.common.entity.User;
+import org.apache.ibatis.annotations.Mapper;
+
+
+@Mapper
+public interface UserInformationMapper extends BaseMapper<User> {
+
+
+}
---
+++ src/main/java/com/shixi3/communitybackend/examine/service/UserInformationService.java
@@ -0,0 +1,10 @@
+package com.shixi3.communitybackend.examine.service;
+
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.shixi3.communitybackend.common.entity.User;
+
+
+public interface UserInformationService extends IService<User> {
+
+}
