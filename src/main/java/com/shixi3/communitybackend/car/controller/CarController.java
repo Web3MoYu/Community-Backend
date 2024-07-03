722967ef95f9439691bb3131ad0ecccd9f22672d
徐世豪 <1304928447@qq.com>
2024-07-03 16:57:04 +0800

    添加、删除、编辑车辆信息

--- src/main/java/com/shixi3/communitybackend/car/controller/CarController.java
+++ src/main/java/com/shixi3/communitybackend/car/controller/CarController.java
@@ -2,14 +2,13 @@ package com.shixi3.communitybackend.car.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.shixi3.communitybackend.car.entity.Car;
+import com.shixi3.communitybackend.car.entity.Parking;
 import com.shixi3.communitybackend.car.service.CarService;
 import com.shixi3.communitybackend.common.model.CommonResult;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -42,6 +41,7 @@ public class CarController {
      * @return
      */
     @GetMapping("/all")
+    @PreAuthorize("hasAnyAuthority('car:cars:list')")
     public CommonResult<List<Car>> getAllCar(){
         List<Car> cars=carService.list();
         System.out.println("测试中");
@@ -51,5 +51,49 @@ public class CarController {
         }else {
             return CommonResult.error(1,"车辆信息获取失败!");
         }
+    }
+    /**
+     * 新增车辆信息
+     * @param car 提交车辆信息
+     * @return
+     */
+    @PostMapping("/add")
+    @PreAuthorize("hasAnyAuthority('car:cars:add')")
+    public CommonResult<String> addCar(@RequestBody Car car){
+        boolean save=carService.save(car);
+        if(save) {
+            return CommonResult.success("新增车辆成功！");
+        }
+        return CommonResult.error(500,"新增车辆失败！");
+    }
+
+    /**
+     * 根据id删除车辆信息
+     * @param id 车辆id
+     * @return
+     */
+    @DeleteMapping("/delete/{id}")
+    @PreAuthorize("hasAnyAuthority('car:cars:delete')")
+    public CommonResult<String> deleteCar(@PathVariable Long id){
+        boolean delete=carService.removeById(id);
+        if(delete){
+            return CommonResult.success("删除车辆成功!");
+        }
+        return CommonResult.error(500,"删除车辆失败!");
+    }
+
+    /**
+     * 编辑车辆信息
+     * @param car 编辑的车辆信息
+     * @return
+     */
+    @PutMapping("/edit")
+    @PreAuthorize("hasAuthority('car:cars:edit')")
+    public CommonResult<String> updateParking(@RequestBody Car car) {
+        boolean update = carService.updateById(car);
+        if(update) {
+            return CommonResult.success("修改车辆信息成功！");
+        }
+        return CommonResult.error(500,"修改车辆信息失败！");
     }
 }
