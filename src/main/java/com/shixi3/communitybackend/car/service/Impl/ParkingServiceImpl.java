9a6df86452c85f7f3816f5ee9613d650eb1b53fa
徐世豪 <1304928447@qq.com>
2024-07-03 10:18:04 +0800

    修复

--- src/main/java/com/shixi3/communitybackend/car/service/Impl/ParkingServiceImpl.java
+++ src/main/java/com/shixi3/communitybackend/car/service/Impl/ParkingServiceImpl.java
@@ -7,6 +7,8 @@ import com.shixi3.communitybackend.car.mapper.CarMapper;
 import com.shixi3.communitybackend.car.mapper.ParkingMapper;
 import com.shixi3.communitybackend.car.service.CarService;
 import com.shixi3.communitybackend.car.service.ParkingService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class ParkingServiceImpl extends ServiceImpl<ParkingMapper,Parking> implements ParkingService{
 }
