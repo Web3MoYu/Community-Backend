75492cb08b2caa097f2e91aea53a8cabaa4993e0
zxin <2932554779@qq.com>
2024-07-03 17:03:18 +0800

    feat:批量删除房屋信息

--- src/main/java/com/shixi3/communitybackend/house/controller/HouseController.java
+++ src/main/java/com/shixi3/communitybackend/house/controller/HouseController.java
@@ -7,6 +7,8 @@ import com.shixi3.communitybackend.house.service.HouseService;
 import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/house")
 public class HouseController {
@@ -81,5 +83,19 @@ public class HouseController {
             return CommonResult.success("删除房屋信息成功！");
         }
         return CommonResult.error(500,"删除房屋信息失败！");
+    }
+
+    /**
+     * 批量删除房屋信息
+     * @param ids 房屋id列表
+     * @return 提示信息
+     */
+    @DeleteMapping("/delBatch")
+    public CommonResult<String> delBatch(@RequestBody List<Long> ids) {
+        boolean delete = houseService.removeBatchByIds(ids);
+        if(delete) {
+            return CommonResult.success("批量删除房屋信息成功！");
+        }
+        return CommonResult.error(500,"批量删除房屋信息失败！");
     }
 }
