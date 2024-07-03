97bdd44a3911cb3fbab39d00117c2600b5b542df
CYQ25 <2153854980@qq.com>
2024-07-03 10:31:43 +0800

    修改异常处理乱码问题

--- src/main/java/com/shixi3/communitybackend/auth/util/ServletUtil.java
+++ src/main/java/com/shixi3/communitybackend/auth/util/ServletUtil.java
@@ -4,11 +4,13 @@ import com.alibaba.fastjson.JSON;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 public class ServletUtil {
     // 写出数据给前端
     public static void writeJSON(HttpServletResponse response, Object object) throws IOException {
         String content = JSON.toJSONString(object);
+        response.setContentType("application/json");
         response.getWriter().write(content);
     }
 }
--- src/main/java/com/shixi3/communitybackend/common/enums/ErrorCodes.java
+++ src/main/java/com/shixi3/communitybackend/common/enums/ErrorCodes.java
@@ -4,24 +4,24 @@ package com.shixi3.communitybackend.common.enums;
 import com.shixi3.communitybackend.common.model.ErrorCode;
 
 public interface ErrorCodes {
-    ErrorCode SUCCESS = new ErrorCode(0, "成功");
+    ErrorCode SUCCESS = new ErrorCode(0, "success");
 
     // ========== 客户端错误段 ==========
-    ErrorCode BAD_REQUEST = new ErrorCode(400, "请求参数不正确");
-    ErrorCode UNAUTHORIZED = new ErrorCode(401, "账号未登录");
-    ErrorCode FORBIDDEN = new ErrorCode(403, "没有该操作权限");
-    ErrorCode NOT_FOUND = new ErrorCode(404, "请求未找到");
-    ErrorCode METHOD_NOT_ALLOWED = new ErrorCode(405, "请求方法不正确");
-    ErrorCode LOCKED = new ErrorCode(423, "请求失败，请稍后重试"); // 并发请求，不允许
-    ErrorCode TOO_MANY_REQUESTS = new ErrorCode(429, "请求过于频繁，请稍后重试");
+    ErrorCode BAD_REQUEST = new ErrorCode(400, "request parameter error");
+    ErrorCode UNAUTHORIZED = new ErrorCode(401, "not login");
+    ErrorCode FORBIDDEN = new ErrorCode(403, "no authorization");
+    ErrorCode NOT_FOUND = new ErrorCode(404, "not found");
+    ErrorCode METHOD_NOT_ALLOWED = new ErrorCode(405, "request method not allowed");
+    ErrorCode LOCKED = new ErrorCode(423, "request failed"); // 并发请求，不允许
+    ErrorCode TOO_MANY_REQUESTS = new ErrorCode(429, "too many requests");
 
     // ========== 服务端错误段 ==========
 
-    ErrorCode INTERNAL_SERVER_ERROR = new ErrorCode(500, "系统异常");
-    ErrorCode NOT_IMPLEMENTED = new ErrorCode(501, "功能未实现/未开启");
+    ErrorCode INTERNAL_SERVER_ERROR = new ErrorCode(500, "system error");
+    ErrorCode NOT_IMPLEMENTED = new ErrorCode(501, "not implemented");
 
     // ========== 自定义错误段 ==========
-    ErrorCode REPEATED_REQUESTS = new ErrorCode(900, "重复请求，请稍后重试"); // 重复请求
-    ErrorCode DEMO_DENY = new ErrorCode(901, "演示模式，禁止写操作");
-    ErrorCode UNKNOWN = new ErrorCode(999, "未知错误");
+    ErrorCode REPEATED_REQUESTS = new ErrorCode(900, "repeated requests"); // 重复请求
+    ErrorCode DEMO_DENY = new ErrorCode(901, "demo deny");
+    ErrorCode UNKNOWN = new ErrorCode(999, "unknown error");
 }
