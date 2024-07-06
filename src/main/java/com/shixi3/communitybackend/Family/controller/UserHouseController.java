cc68a9d335e18b822ee37665ac68986f5494410a
CYQ25 <2153854980@qq.com>
2024-07-06 11:52:36 +0800

    恢复删除家庭成员和批量删除家庭成员

--- src/main/java/com/shixi3/communitybackend/Family/controller/UserHouseController.java
+++ src/main/java/com/shixi3/communitybackend/Family/controller/UserHouseController.java
@@ -3,12 +3,16 @@ package com.shixi3.communitybackend.Family.controller;
 import com.shixi3.communitybackend.Family.entity.UserHouse;
 import com.shixi3.communitybackend.Family.entity.WxUser;
 import com.shixi3.communitybackend.Family.entity.WxUserTree;
+import com.shixi3.communitybackend.Family.mapper.UserHouseMapper;
 import com.shixi3.communitybackend.Family.service.UserHouseService;
+import com.shixi3.communitybackend.Family.vo.DeleteVo;
+import com.shixi3.communitybackend.auth.mapper.UserMapper;
 import com.shixi3.communitybackend.common.entity.Page;
 import com.shixi3.communitybackend.common.exception.BizException;
 import com.shixi3.communitybackend.common.model.CommonResult;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -21,7 +25,8 @@ import java.util.List;
 public class UserHouseController {
     @Resource
     UserHouseService userHouseService;
-
+    @Autowired
+    private UserHouseMapper userHouseMapper;
     /**
      * 获取房屋户主
      * @param id
@@ -122,6 +127,40 @@ public class UserHouseController {
             return CommonResult.success(count);
         }else {
             return CommonResult.error(0,"添加失败");
+        }
+    }
+
+    /**
+     * 删除用户房屋关系表的一条数据
+     * 在前台户主先查询家庭成员后可删除家庭成员（先判断是否为户主）
+     * 后台管理员可执行删除户主和该房屋成员
+     * @param id
+     * @return
+     */
+    @DeleteMapping("/deleteHouseMember/{id}")
+    public CommonResult<Integer> deleteHouseMember(@PathVariable Long id) {
+        int count=userHouseService.deleteHouseMember(id);
+        if(count!=0){
+            return CommonResult.success(count);
+        }else {
+            throw new BizException("删除失败!");
+        }
+    }
+
+    /**
+     * 批量删除用户房屋关系
+     * 后台管理员可删除户主，成员，租户
+     * 前台户主可删除家庭成员
+     * @param
+     * @return
+     */
+    @DeleteMapping("/batchDeleteHouseMember")
+    public CommonResult<String> batchDeleteHouseMember(@RequestBody DeleteVo deleteVo) {
+        int count=userHouseMapper.deleteBatchIds(deleteVo.getIds());
+        if(count!=0){
+            return CommonResult.success("删除成功");
+        }else {
+            throw new BizException("删除失败");
         }
     }
 
