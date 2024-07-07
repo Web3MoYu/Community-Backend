efed5aa0b7c65415ad3a08634167cb74f8327042
徐世豪 <1304928447@qq.com>
2024-07-07 16:41:57 +0800

    删除审核的车辆车位信息

--- src/main/java/com/shixi3/communitybackend/examine/controller/CarVetController.java
+++ src/main/java/com/shixi3/communitybackend/examine/controller/CarVetController.java
@@ -1,15 +1,11 @@
 package com.shixi3.communitybackend.examine.controller;
 
-import com.shixi3.communitybackend.car.entity.Car;
 import com.shixi3.communitybackend.common.model.CommonResult;
 import com.shixi3.communitybackend.examine.entity.CarVet;
 import com.shixi3.communitybackend.examine.service.CarVetService;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,10 +17,29 @@ public class CarVetController {
     @Resource
     private CarVetService carVetService;
 
+    /**
+     * 根据审核状态查询车辆审核表
+     * @param status 审核状态
+     * @return 车辆信息表
+     */
     @GetMapping("/getCar/{status}")
     public CommonResult<List<CarVet>> getCarVetByStatus(@PathVariable Integer status){
         List<CarVet> carVets=carVetService.getCarVetByStatus(status);
         System.out.println(carVets);
         return CommonResult.success(carVets);
+    }
+
+    /**
+     * 根据id删除审核车辆信息
+     * @param id 审核id
+     * @return 提示信息
+     */
+    @DeleteMapping("/delete/{id}")
+    public CommonResult<String> deleteCar(@PathVariable Long id){
+        boolean delete=carVetService.removeById(id);
+        if(delete){
+            return CommonResult.success("删除车辆成功!");
+        }
+        return CommonResult.error(500,"删除车辆失败!");
     }
 }
--- src/main/java/com/shixi3/communitybackend/examine/controller/ParkingVetController.java
+++ src/main/java/com/shixi3/communitybackend/examine/controller/ParkingVetController.java
@@ -5,10 +5,7 @@ import com.shixi3.communitybackend.examine.entity.ParkingVet;
 import com.shixi3.communitybackend.examine.service.ParkingVetService;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,14 +17,28 @@ public class ParkingVetController {
     private ParkingVetService parkingVetService;
 
     /**
-     * 根据审核状态查询车辆审核表
+     * 根据审核状态查询车位审核表
      * @param status 审核状态
-     * @return 车辆信息表
+     * @return 车位信息表
      */
     @GetMapping("/getParking/{status}")
 //    @PreAuthorize("hasAuthority('')")
     public CommonResult<List<ParkingVet>> getParkingVetByStatus(@PathVariable Integer status){
         List<ParkingVet> parkingVets=parkingVetService.getParkingVetByStatus(status);
         return CommonResult.success(parkingVets);
+    }
+
+    /**
+     * 根据审核id删除车位
+     * @param id 审核车位编号
+     * @return 提示信息
+     */
+    @DeleteMapping("/delete/{id}")
+    public CommonResult<String> deleteParking(@PathVariable Long id){
+        boolean delete=parkingVetService.removeById(id);
+        if(delete){
+            return CommonResult.success("删除车位成功!");
+        }
+        return CommonResult.error(500,"删除车位失败!");
     }
 }
