8bd1e28b88fb25a3c0e3b2fef8050307d100f4c4
lsh <3040114965@qq.com>
2024-07-10 09:26:26 +0800

    修改修改wx用户的Api

--- src/main/java/com/shixi3/communitybackend/wxapp/controller/WxPersonalController.java
+++ src/main/java/com/shixi3/communitybackend/wxapp/controller/WxPersonalController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
-@RequestMapping("/wx")
+@RequestMapping("/wx/personal")
 public class WxPersonalController {
 
     @Resource
