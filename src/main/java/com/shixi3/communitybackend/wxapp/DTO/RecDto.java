acf94408eb52c60b5d1158cfae0bb7f5c028db8b
lsh <3040114965@qq.com>
2024-07-11 17:19:10 +0800

    车牌比对信息

--- src/main/java/com/shixi3/communitybackend/wxapp/DTO/RecDto.java
+++ src/main/java/com/shixi3/communitybackend/wxapp/DTO/RecDto.java
@@ -3,7 +3,9 @@ package com.shixi3.communitybackend.wxapp.DTO;
 import lombok.Data;
 
 @Data
-public class FaceRecDto {
+public class RecDto {
     private String image1;
     private String image2;
+
+    private String image;
 }
--- src/main/java/com/shixi3/communitybackend/wxapp/controller/ImageRecognitionController.java
+++ src/main/java/com/shixi3/communitybackend/wxapp/controller/ImageRecognitionController.java
@@ -2,11 +2,10 @@ package com.shixi3.communitybackend.wxapp.controller;
 
 import com.alibaba.fastjson.JSON;
 import com.shixi3.communitybackend.common.model.CommonResult;
-import com.shixi3.communitybackend.wxapp.DTO.FaceRecDto;
+import com.shixi3.communitybackend.wxapp.DTO.RecDto;
 import okhttp3.*;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import okhttp3.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 
@@ -16,15 +15,18 @@ public class ImageRecognitionController {
     public static final String API_KEY = "Gc8IiLT2vQrJS2HpnVg44bDh";
     public static final String SECRET_KEY = "Y4oIYc63ce0AEmyZ7ZYSw4yen06UaJ9L";
 
+    public static final String WORDS_API_KEY = "FbZKXHIWMFMDOVNOZ3hgz9WD";
+    public static final String WORDS_SECRET_KEY = "3j5wbG7fv7rYIEiJgrbZfVHUbgUb1oP4";
+
     OkHttpClient HTTP_CLIENT = new OkHttpClient().newBuilder().build();
 
 
     @PostMapping("/faceRec")
-    public CommonResult<String> faceRec(@org.springframework.web.bind.annotation.RequestBody FaceRecDto faceRecDto) throws IOException {
+    public CommonResult<String> faceRec(@org.springframework.web.bind.annotation.RequestBody RecDto faceRecDto) throws IOException {
         MediaType mediaType = MediaType.parse("application/json");
         RequestBody body = RequestBody.create(mediaType, "[{\"image\":\"" + faceRecDto.getImage1() + "\",\"image_type\":\"BASE64\"},{\"image\":\"" + faceRecDto.getImage2() + "\",\"image_type\":\"BASE64\"}]");
         Request request = new Request.Builder()
-                .url("https://aip.baidubce.com/rest/2.0/face/v3/match?access_token=" + getAccessToken())
+                .url("https://aip.baidubce.com/rest/2.0/face/v3/match?access_token=" + getAccessToken(API_KEY, SECRET_KEY))
                 .method("POST", body)
                 .addHeader("Content-Type", "application/json")
                 .build();
@@ -32,10 +34,24 @@ public class ImageRecognitionController {
         return CommonResult.success(response.body().string());
     }
 
-    private String getAccessToken() throws IOException {
+    @PostMapping("/carRec")
+    public CommonResult<String> carRec(@org.springframework.web.bind.annotation.RequestBody RecDto recDto) throws IOException {
         MediaType mediaType = MediaType.parse("application/x-www-form-urlencoded");
-        RequestBody body = RequestBody.create(mediaType, "grant_type=client_credentials&client_id=" + API_KEY
-                + "&client_secret=" + SECRET_KEY);
+        RequestBody body = RequestBody.create(mediaType, "image=" + recDto.getImage() + "&detect_direction=false&paragraph=false&probability=false");
+        Request request = new Request.Builder()
+                .url("https://aip.baidubce.com/rest/2.0/ocr/v1/accurate?access_token=" + getAccessToken(WORDS_API_KEY, WORDS_SECRET_KEY))
+                .method("POST", body)
+                .addHeader("Content-Type", "application/x-www-form-urlencoded")
+                .addHeader("Accept", "application/json")
+                .build();
+        Response response = HTTP_CLIENT.newCall(request).execute();
+        return CommonResult.success(response.body().string());
+    }
+
+    private String getAccessToken(String apiKey, String secretKey) throws IOException {
+        MediaType mediaType = MediaType.parse("application/x-www-form-urlencoded");
+        RequestBody body = RequestBody.create(mediaType, "grant_type=client_credentials&client_id=" + apiKey
+                + "&client_secret=" + secretKey);
         Request request = new Request.Builder()
                 .url("https://aip.baidubce.com/oauth/2.0/token")
                 .method("POST", body)
